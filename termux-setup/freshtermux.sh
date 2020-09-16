908459c56a3ff6eda26cdf964a018f187ac76ce8
RebelLion420 <gaigecarlos@gmail.com>
2020-09-15 19:09:23 -0700

    termux: update setup

--- termux-setup/freshtermux.sh
+++ termux-setup/freshtermux.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/bash
 BASEDIR=$(pwd)
-for PKG in $(cat $BASEDIR/termux.pkgs); do
-    apt list -a $PKG 2&>/dev/null || {
-        echo $PKG >> $BASEDIR/pkgs.txt
-    }
-done
-yes|xargs -a $BASEDIR/pkgs.txt pkg install
-rm $BASEDIR/pkgs.txt
+yes|pkg update
+yes|xargs -a $BASEDIR/termux.pkgs
 termux-setup-storage
 wget https://raw.githubusercontent.com/SDRausty/termux-arch/master/setupTermuxArch.bash
 bash setupTermuxArch.bash
