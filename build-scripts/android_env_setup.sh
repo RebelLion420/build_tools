6faa08a8dfa935ed8d09aaa6d9d171b06aca7d40
RebelLion420 <gaigecarlos@gmail.com>
2024-10-12 23:36:07 -0700

    Update android_env_setup.sh
    Attempt #2

--- build-scripts/android_env_setup.sh
+++ build-scripts/android_env_setup.sh
@@ -11,15 +11,15 @@ if [[ "${BASH_SOURCE[0]}" != "$(basename -- "$0")" ]]; then
     echo -e "\n${red}Do not source this script!\n\nUsage:${rst} bash $(basename -- "$0")\n"
     kill -INT $$
 fi
-[[ $EUID -ne 0 ]] && echo "This script must be run as root." && exit 1
+#[[ $EUID -ne 0 ]] && echo "This script must be run as root." && exit 1
 echo ""
 read -rp $'\e[1;92m:: Enter user name:\e[0m ' USER
 HOME="/home/$USER"
 TMP="$HOME/.tmp"
 ANDROID_DIR="$HOME/Android"
 cd "$HOME"
-sudo -u "$USER" mkdir -p "$TMP"
-sudo -u "$USER" mkdir -p "$ANDROID_DIR"
+mkdir -p "$TMP"
+mkdir -p "$ANDROID_DIR"
 if [ -f "$TMP"/count* ]; then
     msg "Setup already ran before."
     msg "You may need to run some commands manually"
@@ -28,8 +28,8 @@ if [ -f "$TMP"/count* ]; then
 fi
 if [ ! -d "$ANDROID_DIR"/scripts/setup ]; then
 	msg "Cloning setup scripts..."
-	sudo -u "$USER" mkdir "$ANDROID_DIR"/scripts
-	sudo -u "$USER" git clone https://github.com/RebelLion420/build_scripts "$ANDROID_DIR"/scripts
+	mkdir "$ANDROID_DIR"/scripts
+	git clone https://github.com/RebelLion420/build_scripts "$ANDROID_DIR"/scripts
 fi
 msg "Setup scripts acquired"
 if [ ! -f "$TMP"/count1 ]; then
@@ -37,31 +37,31 @@ if [ ! -f "$TMP"/count1 ]; then
 	msg "Setting up Android Build Environment..."
 	bash setup/arch-manjaro.sh || die "Arch-Manjaro" && exit 1
 	bash setup/install_android_sdk.sh || die "Android SDK" && exit 1
-	sudo -u "$USER" touch "$TMP"/count1
+	touch "$TMP"/count1
 fi
 msg "Prerequisites installed succesfully."
 if [ ! -f "$TMP"/count2 ]; then
 	msg "Installing ccache..."
 	bash setup/ccache.sh || die "ccache"
-	sudo -u "$USER" touch "$TMP"/count2
+	touch "$TMP"/count2
 fi
 msg "ccache installed succesfully"
 if [ ! -f "$TMP"/count3 ]; then
 	msg "Installing make..."
 	bash setup/make.sh || die "make"
-	sudo -u "$USER" touch "$TMP"/count3
+	touch "$TMP"/count3
 fi
 msg "make installed succesfully"
 if [ ! -f "$TMP"/count4 ]; then
 	msg "Installing ninja..."
 	bash setup/ninja.sh || die "ninja"
-	sudo -u "$USER" touch "$TMP"/count4
+	touch "$TMP"/count4
 fi
 msg "ninja installed succesfully"
 if [ ! -f "$TMP"/count5 ]; then
 	msg "Installing hub..."
 	bash setup/hub.sh || die "hub"
-	sudo -u "$USER" touch "$TMP"/count5
+	touch "$TMP"/count5
 fi
 msg "hub installed succesfully"
 sleep 1
