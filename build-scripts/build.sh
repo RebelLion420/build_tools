57acd83134c9417acc0f9d4c1ecd552388362f6b
RebelLion420 <gaigecarlos@gmail.com>
2024-10-19 17:51:34 -0700

    update build script

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -16,10 +16,11 @@ kernrel=/Root/RK/Release
 #
 # Directories
 #
-kdir="${PWD}"
-ak3="${kdir}"/AK3
-out="${kdir}"/out
-kernels="${kdir}"/../kernels
+kdir="$PWD"
+ak3="$kdir"/AK3
+out="$kdir"/out
+logs="$kdir/logs"
+kernels="$kdir"/../kernels
 #
 # Colors
 #
@@ -70,11 +71,12 @@ usage() {
 	echo ""
 }
 greplog() {
-	if [ -f .errors ]; then
-		grep -Ei 'fail|fatal|cannot' .errors < .errorlog
-		awk '/error: /{flag=1} /cc1: /{flag=0} flag' .errors < .errorlog
-		printf "${top}\n${mid}%s\n${mid}\n${end}\n\n" "$(sed -e 's/^/\	║	/'.errorlog)"
-	else
+	if [ -f .errors_* ]; then
+		grep -Ei 'error|fail|fatal|cannot' .errors_* >> .errorlog 
+#		awk '/error: /{flag=1} /cc1: /{flag=0} flag' .errors_* < .errorlog 
+		printf "${top}\n${mid}%s\n${mid}\n${end}\n\n" "$(sed -e 's/^/\	║	/' .errorlog)"
+        mv .errors_* "$logs"/ 
+    else
 		fdie "No log captured"
 	fi
 }
@@ -86,17 +88,17 @@ runtime() {
 }
 lsconfigs() {
 	fdie "Target device not found. Choose a valid defconfig."
-	if [ ${arch} = arm ]; then
+	if [ "$arch" = arm ]; then
 		ferr "Listing available configs:"
 		find arch/arm/configs -maxdepth 1 -mindepth 1 -name "*_defconfig"|sed -e 's/^/\	/'|sort
-	elif [ ${arch} = arm64 ]; then
+	elif [ "$arch" = arm64 ]; then
 		ferr "Listing available configs:"
 		find arch/arm64/configs -maxdepth 1 -mindepth 1 -name "*_defconfig"|sed -e 's/^/\	/'|sort
 	fi
 }
 run_build() {
 	cmd="make ${1}"
-	make O="${out}" makeflags="${flags}" -j"${threads}" "${@}"
+	make O="$out" makeflags="$flags" -j"$threads" "${@}"
 	if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
 		fdie "${cmd} failed, compilation terminated"
 		greplog
@@ -135,28 +137,28 @@ shift $((OPTIND - 1))
 #
 # Consistency Checks
 #
-[[ -f "${kdir}"/.errors ]] && rm -f "${kdir}"/.errors "${kdir}"/.errorlog
-[[ ! -d "${out}" ]] && mkdir "${out}"
-[[ ! -d "${HOME}"/.local/tmp ]] && mkdir "${HOME}"/.local/tmp
-[[ ! -d "${kernels}" ]] && mkdir "${kernels}"
+[[ -f "$kdir"/.errors ]] && rm -f "$kdir"/.errors_* "$kdir"/.errorlog
+[[ ! -d "$out" ]] && mkdir "$out"
+[[ ! -d "$HOME"/.local/tmp ]] && mkdir "$HOME"/.local/tmp
+[[ ! -d "$kernels" ]] && mkdir "$kernels"
 #
 # Preconfigured Variables
 #
-tc="${kdir}"/../toolchain
+tc="$kdir"/../toolchain
 date=$(date +"%m%d%y")
-tmp="${HOME}"/.local/tmp
-if [ -z "${type}" ] || [ -z "${kname}" ]; then
-	final="${device}"-kernel-"${date}".zip
-elif [ -z "${type}" ]; then
-	final="${kname}"-"${date}".zip
+tmp="$HOME"/.local/tmp
+if [ -z "$type" ] || [ -z "$kname" ]; then
+	final="${device}-kernel_${date}".zip
+elif [ -z "$type" ]; then
+	final="$kname-$device_$date".zip
 else
-	final="${kname}"-"${type}"-"v${version}"-"${date}".zip
+	final="$kname-$device-$type-v$version_$date".zip
 fi
-case ${arch} in
+case "$arch" in
 	"arm")
 		img=zImage
 		export ARCH=arm
-		[ -d "${tc}"/bin ] && export CROSS_COMPILE="${tc}"/bin/arm-linux-gnueabi- || {
+		[ -d "$tc"/bin ] && export CROSS_COMPILE="$tc"/bin/arm-linux-gnueabi- || {
 			fdie "Toolchain not found!"
 			fdie "Move or symlink toolchain folder to '<kernel_source>/../toolchain'"
 			exit 1
@@ -165,7 +167,7 @@ case ${arch} in
 	"arm64")
 		img=Image.gz
 		export ARCH=arm64
-		[ -d "${tc}"/bin ] && export CROSS_COMPILE=${tc}/bin/aarch64-linux-gnu- || {
+		[ -d "$tc"/bin ] && export CROSS_COMPILE="$tc"/bin/aarch64-linux-gnu- || {
 			fdie "Toolchain not found!"
 			fdie "Move or symlink toolchain folder to '<kernel_source>/../toolchain'"
 			exit 1
@@ -176,16 +178,16 @@ case ${arch} in
 		exit 1
 		;;
 esac
-if [[ ${jobs} == "all" ]]; then
+if [[ "$jobs" == "all" ]]; then
 	threads=$(($(nproc --all) + 1))
-elif [[ ${jobs} == "n" ]]; then
+elif [[ "$jobs" == "n" ]]; then
 	threads=$(($(nproc --all) - 2))
 else
-	threads="${jobs}"
+	threads="$jobs"
 fi
-export SUBARCH=${arch}
-export DEVICE=${device}
-gccv=$("${CROSS_COMPILE}"gcc -v 2>&1 | tail -1 | cut -d ' ' -f 3)
+export SUBARCH="$arch"
+export DEVICE="$device"
+gccv=$("$CROSS_COMPILE"gcc -v 2>&1 | tail -1 | cut -d ' ' -f 3)
 # Ccache
 if command -v ccache >/dev/null 2>&1; then
 	export CCACHE_SLOPPINESS=file_macro,locale,time_macros,pch_defines
@@ -194,43 +196,44 @@ if command -v ccache >/dev/null 2>&1; then
     export PATH="/usr/lib/ccache/bin/:$PATH"
 fi
 # Checksum
-[[ -f "${out}"/defconfsha ]] && defconfsha=$(awk '{print $1}' "${out}"/defconfsha)
+[[ -f "$out"/defconfsha ]] && defconfsha=$(awk '{print $1}' "$out"/defconfsha)
 #
 # Begin Build Process
 #
 start=$(date +%s)
-printf "\n%s" "${top}"
-fargs "ARCHITECTURE:" "${arch}"
-fargs "DEVICE:" "${device}"
-fargs "THREADS:" "${threads}"
-[ ! -z ${type} ] && fargs "TYPE:" "${type}"
-[ ! -z ${version} ] && fargs "VERSION:" "${version}"
-fargs "GCC VERSION:" "${gccv}"
+printf "\n%s" "$top"
+fargs "ARCHITECTURE:" "$arch"
+fargs "DEVICE:" "$device"
+fargs "THREADS:" "$threads"
+[ ! -z "$type" ] && fargs "TYPE:" "$type"
+[ ! -z "$version" ] && fargs "VERSION:" "$version"
+fargs "GCC VERSION:" "$gccv"
 printf "\n%s\n" "${mid}${rst}"
 printf "${mid}%5s${grn}%-25s ${cyn}%s\n${end}\n${rst}" "" "Build script by " "RebelLion420"
 sleep 1
 fmsg "Building Kernel Image"
-if [[ -f ${out}/.config ]]; then
+if [[ -f "$out"/.config ]]; then
 	if echo "${defconfsha} arch/${arch}/configs/${device}_defconfig"|sha1sum -c --quiet; then
 		fmsg "Skipping generate config"
     fi
 else
-	if make O="${out}" "${device}"_defconfig 2> >(tee .errors >&2); then
+	if make O="$out" "$device"_defconfig 2> >(tee .errors_$date >&2); then
 		fmsg "make ${device}_defconfig succeeded."
-		shasum arch/"${arch}"/configs/"${device}"_defconfig > "${out}"/defconfsha
+		shasum arch/"$arch"/configs/"$device"_defconfig > "$out"/defconfsha
 	else
 		fdie "make ${device}_defconfig failed."
 		greplog
 		exit 1
 	fi
 fi
-run_build "${img}" 2> >(tee -a .errors >&2)
+run_build "$img" 2> >(tee -a .errors_$date >&2)
 fmsg "Building DTBs"
-run_build dtbs 2> >(tee -a .errors >&2)
+run_build dtbs 2> >(tee -a .errors_$date >&2)
 fmsg "Building Modules"
-run_build modules 2> >(tee -a .errors >&2)
+run_build modules 2> >(tee -a .errors_$date >&2)
+find ./ -name "*.ko" -exec ${CROSS_COMPILE}strip --strip-unneeded {} \;
+find ./ -name "*.ko" -exec "$out"/scripts/sign-file sha512 out/certs/signing_key.pem out/certs/signing_key.x509 {} \;
 echo ""
-run_build modules_install INSTALL_MOD_PATH=modinstall INSTALL_MOD_STRIP=1 2> >(tee -a .errors >&2)
 printf "\n${top}\n${mid}\n${mid}%5s ${blinkcyn}%s${rst}\n${mid}\n${end}\n" "" "Build Process Complete"
 runtime
 #
@@ -238,36 +241,36 @@ runtime
 #
 # Sanity Check
 #
-for f in "${ak3}"/*Image*; do
-[[ -e "${f}" ]] && {
-        rm -f "${ak3}"/*Image*
-        rm -f "${ak3}"/*.zip
-        rm -f "${ak3}"/*.dtb
-        rm -f "${ak3}"/modules/system/lib/modules/*.ko
+for f in "$ak3"/*Image*; do
+[[ -e "$f" ]] && {
+        rm -f "$ak3"/*Image*
+        rm -f "$ak3"/*.zip
+        rm -f "$ak3"/*.dtb
+        rm -rf "$ak3"/modules/system/lib/modules/
 }
 done
 #
 # Zip Process
 #
 fmsg "Finding Modules"
-if find "${out}"/modinstall/ -name '*.ko' -type f -exec cp '{}' "${ak3}/modules/system/lib/modules/" \; ; then
+if find ./ -name "*.ko" -exec cp '{}' "${ak3}/modules/system/lib/modules/" \; ; then
 	fmsg "Modules found"
 else
 	ferr "No modules found!"
 fi
 fmsg "Finding DTBs"
-if find "${out}"/arch/${arch}/boot/dts/ -name '*.dtb' -type f -exec cp '{}' "${ak3}/" \; ; then
+if find "$out"/arch/"$arch"/boot/dts/ -name '*.dtb' -type f -exec cp '{}' "$ak3/" \; ; then
 	fmsg "DTBs found"
 else
 	ferr "No DTBs found!"
 fi
 fmsg "Creating Flashable Zip"
-cp  "${out}"/arch/${arch}/boot/${img} "${ak3}"
+cp  "$out"/arch/"$arch"/boot/"$img" "$ak3"
 #cp  "${out}"/arch/${arch}/boot/dts/qcom/*"${device}"*.dtb "${ak3}"
-cd "${ak3}"
-if zip -r9 "${final}" ./* -x .git README.md *placeholder > /dev/null; then
+cd "$ak3"
+if zip -r9 "$final" ./* -x .git README.md *placeholder > /dev/null; then
 	fmsg "Flashable Zip Created"
-    cp "${final}" "${kernels}"
+    cp "$final" "$kernels"
     fmsg "Kernel zip copied to ${kernels}"
 else
 	fdie "Zip Creation Failed"
@@ -276,30 +279,30 @@ fi
 #
 # Upload to MEGA
 #
-if command -v megaput >/dev/null 2>&1 || command -v megatools >/dev/null 2>&1 && [[ -f ${final} ]]; then
+if command -v megaput >/dev/null 2>&1 || command -v megatools >/dev/null 2>&1 && [[ -f "$final" ]]; then
 	echo ""
 	read -rp "Upload kernel zip to MEGA?  " ul
 	if [ "${ul,,}" == "y" ]; then
 		fmsg "Uploading kernel zip to MEGA"
-		case ${type} in
+		case "$type" in
 			"test*")
 				fmsg "${final} --> ${kerntest}"
-				megaput --path ${kerntest} "${final}"
+				megaput --path "$kerntest" "$final"
 				;;
 			"beta*")
 				fmsg "${final} --> ${kernbeta}"
-				megaput --path ${kernbeta} "${final}"
+				megaput --path "$kernbeta" "$final"
 				;;
 			"release*")
 				fmsg "${final} --> ${kernrel}"
-				megaput --path ${kernrel} "${final}"
+				megaput --path "$kernrel" "$final"
 				;;
 			*)
 				fmsg "${final} --> ${kern}"
-				megaput --path ${kern} "${final}"
+				megaput --path "$kern" "$final"
 				;;
 		esac
-		if [ ${?} != 0 ]; then
+		if [ "$?" != 0 ]; then
 			fdie "Upload failed"
 			exit 1
 		else
